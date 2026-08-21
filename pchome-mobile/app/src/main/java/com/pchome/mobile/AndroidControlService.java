fb1c04ecee8509bcfc32dba26c933bc5609026c2
colgecen <alperencolgecen@gmail.com>
2026-08-21 19:14:56 +0300

    fix(mobile): expose AccessibilityService instance and fix key injection
    Adds a static instance set in onServiceConnected so activities can reach the live service. injectKeyEvent now maps KeyEvent codes to GLOBAL_ACTION_* constants instead of passing raw keycodes to performGlobalAction (tasks 45, 46).

--- pchome-mobile/app/src/main/java/com/pchome/mobile/AndroidControlService.java
+++ pchome-mobile/app/src/main/java/com/pchome/mobile/AndroidControlService.java
@@ -12,6 +12,17 @@ import androidx.annotation.RequiresApi;
 
 public class AndroidControlService extends AccessibilityService {
     private static final String TAG = "AndroidControlService";
+    private static AndroidControlService instance;
+
+    public static AndroidControlService getInstance() {
+        return instance;
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        instance = this;
+    }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -24,10 +35,30 @@ public class AndroidControlService extends AccessibilityService {
     }
 
     public boolean injectKeyEvent(int keyCode, boolean pressed) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            return performGlobalAction(keyCode);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            return false;
         }
-        return false;
+        int globalAction;
+        switch (keyCode) {
+            case android.view.KeyEvent.KEYCODE_BACK:
+                globalAction = GLOBAL_ACTION_BACK;
+                break;
+            case android.view.KeyEvent.KEYCODE_HOME:
+            case android.view.KeyEvent.KEYCODE_HOME_PAGE:
+                globalAction = GLOBAL_ACTION_HOME;
+                break;
+            case android.view.KeyEvent.KEYCODE_APP_SWITCH:
+                globalAction = GLOBAL_ACTION_RECENTS;
+                break;
+            case android.view.KeyEvent.KEYCODE_NOTIFICATION:
+                globalAction = GLOBAL_ACTION_NOTIFICATIONS;
+                break;
+            default:
+                Log.w(TAG, "Unsupported key injection for keyCode " + keyCode
+                        + "; AccessibilityService cannot inject arbitrary keys");
+                return false;
+        }
+        return performGlobalAction(globalAction);
     }
 
     public boolean click(float x, float y) {
